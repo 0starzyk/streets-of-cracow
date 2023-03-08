f3ae08c0ed27dced32ee64caa7a0357ad88352df
0starzyk <starzyk@student.agh.edu.pl>
2023-03-08 14:21:01 +0100

    Printing table data

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from bs4 import BeautifulSoup
 import requests
 
 if __name__ == '__main__':
-    source = requests.get('https://www.kodypocztowe.info/krakow')
+    source = requests.get('https://www.kodypocztowe.info/krakow/page:1')
     soup = BeautifulSoup(source.content, 'html.parser')
-    print(soup.prettify())
+    table = soup.find_all('tr', class_='_data')
+    for row in table:
+        text_row = filter(lambda x: '\n' not in x, map(lambda x: x.text, row))
+        print(list(text_row))
