0994b5c45ae6b846e2fd770a6f44c2795d580211
0starzyk <starzyk@student.agh.edu.pl>
2023-03-08 13:44:34 +0100

    Setting up

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-
+from bs4 import BeautifulSoup
+import requests
 
 if __name__ == '__main__':
-    print('Hell')
-
+    source = requests.get('https://www.kodypocztowe.info/krakow')
+    soup = BeautifulSoup(source.content, 'html.parser')
+    print(soup.prettify())
