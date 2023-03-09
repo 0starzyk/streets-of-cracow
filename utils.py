9f8ef81beed3110a5a98735e30b9c46995ae9a32
0starzyk <starzyk@student.agh.edu.pl>
2023-03-09 12:42:55 +0100

    Cleaning up

--- utils.py
+++ utils.py
@@ -1,15 +1,6 @@
 import config
 
 
-# def create_street_table_query():
-#     return '''CREATE TABLE streets (
-# id SERIAL PRIMARY KEY,
-# postal_code VARCHAR NOT NULL,
-# district  VARCHAR NOT NULL,
-# street VARCHAR NOT NULL,
-# numbers VARCHAR
-# );'''
-
 def get_connection_string(password):
     return config.connection_string.replace('***', password)
 
